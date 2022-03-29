af35c45b7d5ec73582b6dbe0ed3765428dc61e71
Max <imaksus15@gmail.com>
2022-03-29 20:22:38 +0300

    Fek

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading;
+
+namespace ghoul
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1000; i > 0; i -= 7)
+            {
+                Console.WriteLine(i + " - 7 = " + (i-7));
+                Thread.Sleep(20);
+            }
+        }
+    }
+}
